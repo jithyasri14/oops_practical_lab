ebef0fdee26fa53cbe45b1c48508bd845a2705fd
jithyasri14 <jithyasri06@gmail.com>
2025-08-23 08:24:58 +0530

    Add files via upload

---
+++ bitwise2.java
@@ -0,0 +1,15 @@
+package oop_practical;
+import java.util.Scanner;
+public class bitwise2 {
+	public static void main(String args[]) {
+	      int a,b,c;
+	      Scanner sc= new Scanner(System.in);
+	      a= sc.nextInt();
+	      b= sc.nextInt();
+	      c= sc.nextInt();
+	      c = a ^ b;
+	      System.out.println("a ^ b = " + c );
+	      c = ~a; 
+	      System.out.println("~a = " + c );
+	   }
+}
