60b101c3b73a3201d9f7c69f6bd0099dd38aa931
jithyasri14 <jithyasri06@gmail.com>
2025-08-23 08:26:38 +0530

    Add files via upload

---
+++ default_constructor.java
@@ -0,0 +1,33 @@
+package oop_practical;
+class Boxnew {  
+    double width;  	
+    double height;  
+    double depth; 
+	 
+    Boxnew() {
+        System.out.println("Constructing Box"); 
+        width = 10;
+        height = 10; 
+        depth = 10;
+    }
+	
+    double volume() {
+        return width * height * depth;
+    }
+}
+
+public class default_constructor {
+	public static void main(String args[]) { 
+        // declare, allocate, and initialize Boxnew objects 
+        Boxnew mybox1 = new Boxnew();
+        Boxnew mybox2 = new Boxnew(); 
+        double vol; 
+
+        // get volume of first box 
+        vol = mybox1.volume();
+        System.out.println("Volume is " + vol);
+        // get volume of second box 
+        vol = mybox2.volume(); 
+        System.out.println("Volume is " + vol); 
+    }
+}
