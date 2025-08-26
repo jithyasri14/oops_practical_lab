e0d7cd7a808f5d91db2eda105ffa0908d41f2c39
jithyasri14 <jithyasri06@gmail.com>
2025-08-26 12:22:26 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,11 @@
+package oop_practical;
+import java.util.Scanner;
+public class Ternary {
+	public static void main(String[] args) {
+	     int februaryDays = 29;
+	     Scanner sc= new Scanner(System.in);
+	     String result;
+	     result = (februaryDays == 28) ? "Not a leap year" : "Leap year";
+	     System.out.println(result);
+	   }
+}
